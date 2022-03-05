47b18903a9ee7cd2be021e90f8ca28b4217086cf
esckeyboard <70459924+esckeyboard@users.noreply.github.com>
2022-03-05 19:14:13 +0700

    Update index.py

--- index.py
+++ index.py
@@ -46,6 +46,64 @@ def data():
 </rss>
 '''
 
+@sch.task('cron', id='datapop', second='*/20')
+def datapop():
+ 
+  today = datetime.datetime.now()
+  tm = today.strftime('%a, %d %b %y %H:%M:%S GMT')
+  print("============ DATA UPDATED ============")
+  print(tm)
+  data1 = {}
+  url = requests.get('https://nhentai.net/search/?q=english+-guro+-amputee&page=1&sort=popular-today')
+  soup = BeautifulSoup(url.content, 'html')
+  contents = soup.find('div', attrs = {'class':'container index-container'})
+
+#  f = open("main.xml", "w")
+#  print('<?xml version="1.0" encoding="utf-8"?>\n<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">\n<channel>\n<title>nhentai-api</title>\n<lastBuildDate>', tm, '</lastBuildDate>\n',file=f)
+  a1 = '<?xml version="1.0" encoding="utf-8"?>\n<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">\n<channel>\n<title>nhentai-api</title>\n<lastBuildDate>', tm, '</lastBuildDate>\n'
+
+  for content in contents :
+    title1 = content.text
+    link = content.a['href']
+    title = re.sub(r'[@$&]+','', title1)
+    konten = f"""<item>
+  <title>{title}</title>
+  <link>https://nhentai.net{link}</link>
+</item>"""
+#    print(konten, file=f)
+    a2 = konten
+    data1[title] = f"https://nhentai.net{link}"
+  
+  nl = "\n"
+  print(data1)
+  a3 = "\n</channel>\n</rss>" 
+#  print("</channel>\n</rss>", file=f)
+#  f.close()
+  return f'''<?xml version="1.0" encoding="utf-8"?>
+<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+<channel>
+<title>nhentai-eng-api</title>
+<lastBuildDate>{tm}</lastBuildDate>
+{nl.join(f'<item>{nl} <title>{key}</title>{nl} <link>{value}</link>{nl}</item>' for key, value in data1.items())}
+</channel>
+</rss>
+'''
+
+@app.errorhandler(404)
+def page_not_found(e):
+ return '''
+ <hr />
+<h1 style="text-align:center"><span style="font-size:72px"><strong>404 Not Found</strong></span></h1>
+<p style="text-align:center"><span style="font-size:20px">Where are you going ? There is nothing here.. .</span></p>
+<hr />
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p style="text-align:center"><img alt="" src="https://c.tenor.com/nq76xNvyoYkAAAAd/construction-lol.gif" style="height:264px; width:424px" /></p>
+<p style="text-align:center">&nbsp;</p>
+<p style="text-align:center">&nbsp;</p>
+<p style="text-align:center"><span style="font-size:20px">&gt;&gt;&gt;&nbsp;<strong><span style="background-color:#00ff00"><a href="/" style="background-color: #00ff00;">Home</a></span></strong>&nbsp;|&nbsp;<strong><span style="background-color:#00ff00"><a href="/rss" style="background-color: #00ff00;">nHentai English RSS</a></span>&nbsp;</strong>| <strong><span style="background-color:#00ff00"><a href="/about" style="background-color: #00ff00;">About</a></span>&nbsp;</strong>&lt;&lt;&lt;&lt;</span></p>
+'''
+ 
 @app.route('/')
 def main():
   return '''
@@ -94,28 +152,10 @@ def about():
 
 <p style="text-align:center"><span style="font-size:20px">&gt;&gt;&gt;&nbsp;<strong><span style="background-color:#00ff00"><a href="/" style="background-color: #00ff00;">Home</a></span></strong>&nbsp;|&nbsp;<strong><span style="background-color:#00ff00"><a href="/rss" style="background-color: #00ff00;">nHentai English RSS</a></span>&nbsp;</strong>| <strong><span style="background-color:#00ff00"><a href="/rss-pop" style="background-color: #00ff00;">nHentai Today Popular RSS</a></span>&nbsp;</strong>&lt;&lt;&lt;&lt;</span></p>
 '''
- 
+
 @app.route('/rss-pop')
 def rsspop():
- return '''
- <hr />
-<h1 style="text-align:center"><span style="font-size:72px"><strong>404 Not Found</strong></span></h1>
-
-<p style="text-align:center"><span style="font-size:20px">Still under construction</span></p>
-
-<hr />
-<p>&nbsp;</p>
-
-<p>&nbsp;</p>
-
-<p style="text-align:center"><img alt="" src="https://c.tenor.com/nq76xNvyoYkAAAAd/construction-lol.gif" style="height:264px; width:424px" /></p>
-
-<p style="text-align:center">&nbsp;</p>
-
-<p style="text-align:center">&nbsp;</p>
-
-<p style="text-align:center"><span style="font-size:20px">&gt;&gt;&gt;&nbsp;<strong><span style="background-color:#00ff00"><a href="/" style="background-color: #00ff00;">Home</a></span></strong>&nbsp;|&nbsp;<strong><span style="background-color:#00ff00"><a href="/rss" style="background-color: #00ff00;">nHentai English RSS</a></span>&nbsp;</strong>| <strong><span style="background-color:#00ff00"><a href="/about" style="background-color: #00ff00;">About</a></span>&nbsp;</strong>&lt;&lt;&lt;&lt;</span></p>
-'''
+  return Response(datapop(), mimetype='text/xml')
   
 #if __name__ == '__main__':
 #  sch.start()
